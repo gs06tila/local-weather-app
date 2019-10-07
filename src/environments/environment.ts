db87aa3a25eb022277be24e4ed1f1c3734e2f52d
TimL <tim93larsson@gmail.com>
2019-10-07 20:51:35 +0200

    Add API from openweathermap (get weather by country name)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: '51bcb7c82195924a373742fa44742035',
+  baseUrl: 'http://'
 };
 
 /*
